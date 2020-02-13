14a55ce407d89f2bb510dac22370896b8e453334
Pepijn van de Liefvoort <pepijnvdliefvoort@live.nl>
2020-02-13 20:03:12 +0100

    Add advertisements seeder without relations

---
+++ database/factories/AdvertisementFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Advertisement;
+use Faker\Generator as Faker;
+
+$factory->define(Advertisement::class, function (Faker $faker) {
+    return [
+        'title' => $faker->title,
+        'short_description' => $faker->text(60),
+        'long_description' => $faker->text(1200),
+        'price' => $faker->numberBetween(0, 50),
+        'enable_bidding' => $faker->boolean
+    ];
+});
