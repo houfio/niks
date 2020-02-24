a82f635faa2bcf6a85826572698549d61d462a30
Raul de Heer <raul@supertask.nl>
2020-02-24 22:34:12 +0100

    Update login controller

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@
 use App\User;
 use Faker\Generator as Faker;
 use Illuminate\Database\Eloquent\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
@@ -17,7 +18,7 @@ $factory->define(User::class, function (Faker $faker) {
         'house_number' => $faker->buildingNumber,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => bcrypt($faker->password),
+        'password' => Hash::make('test123'),
         'remember_token' => Str::random(10)
     ];
 });
