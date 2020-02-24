7b626bd820f65ed19d0b3d07e2b28d1d3a8fd866
Raul de Heer <raul@supertask.nl>
2020-02-24 23:06:29 +0100

    Add login throttle

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,18 +3,32 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
+    use ThrottlesLogins;
+
+    protected int $maxAttempts = 3;
+    protected int $decayMinutes = 10;
+
+    protected function username()
+    {
+        return 'email';
+    }
+
     public function authenticate(Request $request)
     {
         $rememberMe = boolval($request->get('rememberMe'));
         $password = $request->get('password');
         $email = $request->get('email');
 
-        if (Auth::attempt(['password' => $password, 'email' => $email, 'approved' => 1], $rememberMe)) {
+        $this->incrementLoginAttempts($request);
+
+        if (!$this->hasTooManyLoginAttempts($request) && Auth::attempt(['password' => $password, 'email' => $email, 'approved' => 1], $rememberMe)) {
+            $this->clearLoginAttempts($request);
             return redirect('/');
         } else {
             return redirect('/login');
