9e9362cce7352505f7a8e89a3078a89de32698e1
houfio <26674115+houfio@users.noreply.github.com>
2020-03-03 20:19:21 +0100

    Add error component

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,11 +4,7 @@
 
 @section('content')
   <div class="content">
-    @if ($errors->any())
-      @foreach ($errors->all() as $error)
-        <div class="error">{{ __($error) }}</div>
-      @endforeach
-    @endif
+    @include('components/errors')
     @include('components/login_form')
   </div>
 @endsection
