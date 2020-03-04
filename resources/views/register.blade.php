868a0ea91a52919afd7081c583e9c3ca1f2067a4
houfio <26674115+houfio@users.noreply.github.com>
2020-03-04 18:42:56 +0100

    Start register page styling

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -4,19 +4,32 @@
 
 @section('content')
   <div class="content">
+    <h1 class="page-heading">
+      {{ __('register.title') }}
+    </h1>
     <x-errors/>
-    <form method="post" action="{{ @action('Auth\RegisterController@register') }}">
+    <form class="two-columns" method="post" action="{{ @action('Auth\RegisterController@register') }}">
       @csrf
-      <label for="firstName">Voornaam:</label><br>
-      <input type="text" id="firstName" name="firstName"><br>
-      <label for="lastName">Achternaam:</label><br>
-      <input type="text" id="lastName" name="lastName">
-      <label for="email">E-mailadres:</label><br>
-      <input type="email" id="email" name="email">
-      <label for="phoneNumber">Telefoonnummer:</label><br>
-      <input type="text" id="phoneNumber" name="phoneNumber">
-      <label for="zipCode">Postcode:</label><br>
-      <input type="text" id="zipCode" name="zipCode">
+      <div class="text-input">
+        <label for="firstName">Voornaam</label>
+        <input type="text" id="firstName" name="firstName" value="{{ old('firstName') }}" required/>
+      </div>
+      <div class="text-input">
+        <label for="lastName">Achternaam</label>
+        <input type="text" id="lastName" name="lastName" value="{{ old('firstName') }}" required/>
+      </div>
+      <div class="text-input">
+        <label for="email">E-mail</label>
+        <input type="email" id="email" name="email" value="{{ old('email') }}" required/>
+      </div>
+      <div class="text-input">
+        <label for="phoneNumber">Telefoonnummer</label>
+        <input type="text" id="phoneNumber" name="phoneNumber" value="{{ old('phoneNumber') }}" required/>
+      </div>
+      <div class="text-input">
+        <label for="zipCode">Postcode</label>
+        <input type="text" id="zipCode" name="zipCode" value="{{ old('zipCode') }}" required/>
+      </div>
       <label for="houseNumber">Huisnummer:</label><br>
       <input type="text" id="houseNumber" name="houseNumber">
       <input type="submit" name="requestAccount">
