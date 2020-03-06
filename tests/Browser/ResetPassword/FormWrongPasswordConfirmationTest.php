7db18b79e3e7414ce82590a8007aca047fcc8617
Raul de Heer <raul@supertask.nl>
2020-03-06 13:10:21 +0100

    Add wrong password confirmation test

---
+++ tests/Browser/ResetPassword/FormWrongPasswordConfirmationTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\PasswordReset;
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+use Throwable;
+
+class FormWrongPasswordConfirmationTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * @test
+     * @throws Throwable
+     */
+    public function testWrongPasswordConfirmation()
+    {
+        $user = factory(User::class)->create([
+            'approved' => true
+        ]);
+
+        $passwordResetData = factory(PasswordReset::class)->make();
+
+        factory(PasswordReset::class)->create([
+            'email' => $user->email,
+            'token' => Hash::make($passwordResetData->token)
+        ]);
+
+        $this->browse(function (Browser $browser) use ($user, $passwordResetData) {
+            $browser->visit("/reset/$passwordResetData->token")
+                ->type('email', $user->email)
+                ->type('password', 'newPassword123')
+                ->type('password_confirmation', 'wrongPassword123')
+                ->press('reset')
+                ->assertGuest();
+        });
+    }
+}
