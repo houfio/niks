b332b8cfc8c0b96aa1ba8520517702775cb57a93
Raul de Heer <raul@supertask.nl>
2020-03-06 11:40:27 +0100

    Add forgot password ui tests

---
+++ tests/Browser/ForgotPasswordTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+use Throwable;
+
+class ForgotPasswordTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * @test
+     * @throws Throwable
+     */
+    public function testForgotPasswordForm()
+    {
+        $user = factory(User::class)->create([
+            'approved' => true
+        ]);
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->visit('/forgot')
+                ->screenshot('pagina')
+                ->type('email', $user->email)
+                ->press('forgot');
+        });
+
+        $this->assertDatabaseHas('password_resets', [
+            'email' => $user->email
+        ]);
+    }
+
+    /**
+     * @test
+     * @throws Throwable
+     */
+    public function testForgotPasswordFormInvalidUser()
+    {
+        $user = factory(User::class)->make();
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->visit('/forgot')
+                ->screenshot('pagina')
+                ->type('email', $user->email)
+                ->press('forgot');
+        });
+
+        $this->assertDatabaseMissing('password_resets', [
+            'email' => $user->email
+        ]);
+    }
+
+    /**
+     * @test
+     * @throws Throwable
+     */
+    public function testForgotPasswordFormNotApprovedUser()
+    {
+        $user = factory(User::class)->create();
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->visit('/forgot')
+                ->screenshot('pagina')
+                ->type('email', $user->email)
+                ->press('forgot');
+        });
+
+        $this->assertDatabaseMissing('password_resets', [
+            'email' => $user->email
+        ]);
+    }
+}
