347e4c32f23ec50e5445a24668c3ef897e4d480a
houfio <26674115+houfio@users.noreply.github.com>
2020-03-08 15:28:59 +0100

    Use proper lang file for password reset

--- resources/lang/nl/resetPassword.php
+++ resources/lang/nl/resetPassword.php
@@ -2,8 +2,5 @@
 
 return [
     'title' => 'Wachtwoord resetten',
-    'password' => 'Wachtwoord',
-    'passwordConfirmation' => 'Wachtwoord bevestiging',
-    'email' => 'E-mailadres',
     'tokenExpired' => 'De reset link is verlopen'
 ];
