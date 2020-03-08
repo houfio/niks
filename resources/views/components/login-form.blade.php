a4cb2802c8bdd51f4361d9e4e34565e7c8d02a54
houfio <26674115+houfio@users.noreply.github.com>
2020-03-08 20:25:57 +0100

    Add password reset button to login

--- resources/views/components/login-form.blade.php
+++ resources/views/components/login-form.blade.php
@@ -15,4 +15,7 @@
   <button type="submit" class="button" name="login">
     {{ __('login.title') }}
   </button>
+  <a class="button transparent" href="{{ url('forgot') }}">
+    Wachtwoord vergeten
+  </a>
 </form>
