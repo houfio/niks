9f93fe9f4af0e89a1c8b251a201fcb6ce2412f88
Raul de Heer <raul@supertask.nl>
2020-03-11 12:08:33 +0100

    Update phone number regex

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,7 +10,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Validator::extend('phone_number', function ($attribute, $value, $parameters, $validator) {
-            return preg_match('/(^\+[0-9]{2}|^\+[0-9]{2}\(0\)|^\(\+[0-9]{2}\)\(0\)|^\([0-9]{3}\)|^00[0-9]{2}|^0)([0-9]{9}$| [0-9]{7}$|[0-9\-\s]{10}$)/', $value);
+            return preg_match('/(^\+[0-9]{2}|^\+[0-9]{2}\(0\)|^\(\+[0-9]{2}\)\(0\)|^00[0-9]{2}|^0)([0-9]{9}$|[0-9\-\s]{10}$)/', $value);
         });
 
         Validator::extend('zip_code', function ($attribute, $value, $parameters, $validator) {
