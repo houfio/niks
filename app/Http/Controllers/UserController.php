80f201dd750dd7406869af9d818bd3464d03ded0
Raul de Heer <raul@supertask.nl>
2020-03-11 12:23:34 +0100

    Add user crud controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UpdateUserRequest;
+use App\User;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return view('user.index', [
+            'users' => User::all()
+        ]);
+    }
+
+    public function show(User $user)
+    {
+        return view('user.show', [
+            'user' => $user
+        ]);
+    }
+
+    public function edit(User $user)
+    {
+        return view('user.update', [
+            'user' => $user
+        ]);
+    }
+
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        //
+    }
+
+    public function destroy(User $user)
+    {
+        $user->delete();
+
+        return redirect('/users');
+    }
+}
