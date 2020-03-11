cfefd91490b75c31d022162f6af9e98c4ce56173
Raul de Heer <raul@supertask.nl>
2020-03-11 10:48:07 +0100

    Add policy for user model

---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class UserPolicy
+{
+    use HandlesAuthorization;
+
+    public function before(?User $user, string $ability)
+    {
+        return optional($user)->is_admin;
+    }
+
+    public function viewAny(User $user)
+    {
+        return false;
+    }
+
+    public function view(User $user, User $model)
+    {
+        return $user->id === $model->id;
+    }
+
+    public function create(?User $user)
+    {
+        return is_null($user);
+    }
+
+    public function update(User $user, User $model)
+    {
+        return $user->id === $model->id;
+    }
+
+    public function delete(User $user, User $model)
+    {
+        return $user->id === $model->id;
+    }
+}
