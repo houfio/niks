e4d0aadf0be6c8bbaf98c5dfdb41743b35860b9f
Saywhatwhat <43953848+Saywhatwhat@users.noreply.github.com>
2020-03-17 11:45:14 +0100

    Add WIP show advertisement

--- resources/views/selectedadvertisement.blade.php
+++ resources/views/selectedadvertisement.blade.php
@@ -16,6 +16,11 @@
         <span class="advertisement-info">
           <span class="made-by"> Gemaakt door: {{$user[0]->first_name}} {{$user[0]->last_name}} </span>
           <span class="create-date"> Gemaakt op: {{$i->created_at}}</span>
+          @if($i->asking === 1)
+            <span class="asking"> Vraagt aan </span>
+          @else
+            <span class="asking"> Biedt aan </span>
+          @endif
         </span>
         <div>
           <img src="{{$i-> path}}" class="w3-round" height="200" width="200" />
@@ -29,10 +34,8 @@
         <h4>
           Beschrijving
         </h4>
-          {{$user}}
           {{$i-> long_description}}
         </p>
-
         <li>{{$i}}</li>
       @endforeach
     </ul>
