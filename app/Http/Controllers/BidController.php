f5080ed23a9e48d5fb51acfe45db197466acf570
Raul de Heer <raul@supertask.nl>
2020-03-17 17:51:19 +0100

    Add functions to bidding controller

--- app/Http/Controllers/BidController.php
+++ app/Http/Controllers/BidController.php
@@ -2,18 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Advertisement;
 use App\Bid;
-use Illuminate\Http\Request;
+use App\Http\Requests\BidRequest;
+use Exception;
 
 class BidController extends Controller
 {
-    public function store(Request $request)
+    public function store(BidRequest $request, Advertisement $advertisement)
     {
-        //
+        $data = $request->validated();
+        $bid = new Bid();
+
+        $bid->bid = $data['bid'];
+
+        $bid->advertisement()->associate($advertisement);
+        $bid->user()->associate($request->user());
+
+        $bid->save();
+
+        return redirect("/advertisements/$advertisement->id");
     }
 
+    /**
+     * @param Bid $bid
+     * @throws Exception
+     */
     public function destroy(Bid $bid)
     {
-        //
+        $bid->delete();
     }
 }
