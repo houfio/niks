d712c43219c3ed001969fe7a69c73383811e95c3
Raul de Heer <raul@supertask.nl>
2020-03-23 13:36:07 +0100

    Fix login messages

--- resources/lang/nl/login.php
+++ resources/lang/nl/login.php
@@ -3,5 +3,6 @@
 return [
     'title' => 'Inloggen',
     'invalid_login' => 'E-mail of wachtwoord incorrect',
-    'timeout' => 'Probeer het over 10 minuten opnieuw'
+    'timeout' => 'Probeer het over 10 minuten opnieuw',
+    'not_approved' => 'Uw account is nog niet goedgekeurd'
 ];
