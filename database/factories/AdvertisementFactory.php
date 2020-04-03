fc7346efa03f8d49e5ba455294a96531893dca16
Pepijn <pepijnvdliefvoort@live.nl>
2020-04-03 12:41:45 +0200

    reverted datatype back to string, changed factory method to generate short_description

--- database/factories/AdvertisementFactory.php
+++ database/factories/AdvertisementFactory.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Factory;
 $factory->define(Advertisement::class, function (Faker $faker) {
     return [
         'title' => 'Taart bakken',
-        'short_description' => $faker->sentence(30),
+        'short_description' => $faker->realText(mt_rand(30, 60)),
         'long_description' => $faker->text(1200),
         'price' => $faker->numberBetween(1, 50),
         'enable_bidding' => $faker->boolean(50),
