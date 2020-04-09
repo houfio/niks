8f548797c0eb16980f39b8ba234335f51cc202b0
Raul de Heer <raul@supertask.nl>
2020-04-09 17:18:37 +0200

    Add update coordinates trait

---
+++ app/Traits/UpdateCoordinates.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Traits;
+
+use App\Services\LocationService;
+
+trait UpdateCoordinates
+{
+    private LocationService $locationService;
+
+    public function __construct(LocationService $locationService)
+    {
+        $this->locationService = $locationService;
+    }
+
+    public function save(array $options = [])
+    {
+        $changed = false;
+        $updatedAttributes = $this->isDirty();
+
+        foreach ($updatedAttributes as $attribute) {
+            if (in_array($attribute, $this->updateCoordinates)) {
+                $changed = true;
+            }
+        }
+
+        if ($changed) {
+            $coordinates = $this->locationService->getCoordinates($updatedAttributes['zipCode'], $updatedAttributes['houseNumber']);
+            $this->latitude = $coordinates->getLatitude();
+            $this->longitude = $coordinates->getLongitude();
+        }
+
+        return parent::save($options);
+    }
+}
