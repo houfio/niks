afba3d3db60039e355c86bc31c26eccd150b3796
Nosaiii <jcv.vanderlee@student.avans.nl>
2020-04-10 17:20:53 +0200

    - Created mail view for requesting an intake for a specific (yet) unapproved user - Created mail view a specific (yet) unapproved user and the inviter for an intake both receive when both parties accept a given date and time for an intake - Created a language file for the intake mail views

---
+++ resources/views/mails/intake_requested.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ __('mails/intake.title') }}</title>
+  </head>
+  <body>
+    <div class="content">
+      <span>{{ __('mails/general.title', ['name' => $invitee->getFullName()]) }}</span>
+      <p>
+        {{ __('mails/intake.requested.paragraphOne') }}
+      </p>
+      <ul>
+        <li>
+          Datum: {{ $date }}
+        </li>
+        <li>
+          Tijd: {{ $time }}
+        </li>
+      </ul>
+      <p>
+        {{ __('mails/intake.requested.paragraphTwo') }}
+        <a href="{{ route('accept_intake', ['user' => $invitee, 'accepted' => true]) }}" class="button">
+          {{ __('mails/intake.accept') }}
+        </a>
+        <a href="{{ route('accept_intake', ['user' => $invitee, 'accepted' => false]) }}" class="button">
+          {{ __('mails/intake.reject') }}
+        </a>
+      </p>
+      <span>
+        {{ __('mails/general.greetings') }}<br/>
+        {{ __('mails/general.team') }}
+      </span>
+    </div>
+  </body>
+</html>
