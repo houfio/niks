be1170869dd1a7a41e77de9bfc7a6096029551c5
Raul de Heer <raul@supertask.nl>
2020-04-14 12:38:51 +0200

    Add filtering for standard things (#59)
    * Add filters to controller index
    
    * Fix search with words
    
    * Add coordinates DTO
    
    * Add location service
    
    * Add update coordinates trait
    
    * Add null check for coordinates
    
    * Fix bugs
    
    * Add lat and long columns to users table
    
    * Fix small bugs and edit factory
    
    * Fix var typo
    
    * Add API key to env for Dusk
    
    * Add changes according to review
    
    Co-authored-by: houfio <26674115+houfio@users.noreply.github.com>

---
+++ app/Models/Coordinates.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+class Coordinates
+{
+    private float $latitude, $longitude;
+
+    public function __construct(float $latitude, float $longitude)
+    {
+        $this->latitude = $latitude;
+        $this->longitude = $longitude;
+    }
+
+    public function getLatitude(): float
+    {
+        return $this->latitude;
+    }
+
+    public function getLongitude(): float
+    {
+        return $this->longitude;
+    }
+}
