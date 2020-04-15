11a96e04c9631adfcaec2ae6e1a71aeb544345e1
Raul de Heer <raul@supertask.nl>
2020-04-15 12:28:29 +0200

    Update models and add migration

---
+++ database/migrations/2020_04_15_101317_make_favorite_pivot_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class MakeFavoritePivotTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('user_favorites', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('advertisements_id')->unsigned();
+            $table->foreign('advertisements_id')->references('id')->on('advertisements')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('user_favorites');
+    }
+}
