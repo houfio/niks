6588a01a3a51a128910698445ea2dc9ff5a884fa
Raul de Heer <raul@supertask.nl>
2020-04-15 13:22:40 +0200

    Add controller with routes

---
+++ app/Http/Controllers/UserFavoriteController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\UserFavorite;
+use Illuminate\Http\Request;
+
+class UserFavoriteController extends Controller
+{
+    public function index(Request $request)
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+
+    public function destroy(UserFavorite $userFavorite)
+    {
+
+    }
+}
