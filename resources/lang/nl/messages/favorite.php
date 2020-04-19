05d13775c2b16022f60a6851489a30127c874b14
Raul de Heer <raul@supertask.nl>
2020-04-19 14:18:00 +0200

    Remove useless message

--- resources/lang/nl/messages/favorite.php
+++ resources/lang/nl/messages/favorite.php
@@ -2,6 +2,5 @@
 
 return [
     'saved' => 'Advertentie opgeslagen als favoriet',
-    'deleted' => 'Advertentie verwijderd als favoriet',
-    'already_favorited' => 'Deze advertentie staat al in uw favorieten'
+    'deleted' => 'Advertentie verwijderd als favoriet'
 ];
