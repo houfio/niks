bd0ef7219b0415b49a0e42f463e1f877c7c6ac03
houfio <26674115+houfio@users.noreply.github.com>
2020-05-08 11:21:02 +0200

    Improve advertisement form layout

--- resources/views/advertisement/create.blade.php
+++ resources/views/advertisement/create.blade.php
@@ -18,18 +18,18 @@
           required
         />
         <x-input
-          name="short_description"
-          :label="__('general/attributes.short_description')"
-          :help="__('views/advertisements.short_description_help')"
-          required
-        />
-        <x-input
           name="price"
           type="number"
           :label="__('general/attributes.price')"
           :help="__('views/advertisements.price_help')"
         />
       </div>
+      <x-input
+        name="short_description"
+        :label="__('general/attributes.short_description')"
+        :help="__('views/advertisements.short_description_help')"
+        required
+      />
       <x-input
         name="long_description"
         type="textarea"
@@ -50,14 +50,14 @@
           <option value="0">{{ __('general/attributes.product') }}</option>
           <option value="1">{{ __('general/attributes.service') }}</option>
         </x-input>
-      </div>
-      <div class="checkbox-input">
-        <input type="checkbox" id="enable_bidding" name="enable_bidding" value="0">
-        <label for="enable_bidding">{{ __('general/attributes.enable_bidding') }}</label>
-      </div>
-      <div class="checkbox-input">
-        <input type="checkbox" id="is_asking" name="is_asking" value="0">
-        <label for="is_asking">{{ __('general/attributes.is_asking') }}</label>
+        <div class="checkbox-input">
+          <input type="checkbox" id="enable_bidding" name="enable_bidding" value="0">
+          <label for="enable_bidding">{{ __('general/attributes.enable_bidding') }}</label>
+        </div>
+        <div class="checkbox-input">
+          <input type="checkbox" id="is_asking" name="is_asking" value="0">
+          <label for="is_asking">{{ __('general/attributes.is_asking') }}</label>
+        </div>
       </div>
       <button class="button" type="submit" name="create">
         {{ __('general/attributes.create') }}
