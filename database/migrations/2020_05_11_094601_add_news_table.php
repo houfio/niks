8942ecf9b0e4c1109b32cad8ebe00b9f98d36f02
Raul de Heer <raul@supertask.nl>
2020-05-11 11:52:55 +0200

    Add migration for news table

---
+++ database/migrations/2020_05_11_094601_add_news_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNewsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 120);
+            $table->longText('content');
+            $table->unsignedBigInteger('creator_id');
+            $table->foreign('creator_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+}
