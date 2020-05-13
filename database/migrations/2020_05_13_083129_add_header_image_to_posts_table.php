49ab35c8d3e9212e1b9ad00ea4cf257bfa7a1653
Raul de Heer <raul@supertask.nl>
2020-05-13 10:34:09 +0200

    Add new column for header image to posts table

---
+++ database/migrations/2020_05_13_083129_add_header_image_to_posts_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddHeaderImageToPostsTable extends Migration
+{
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->unsignedBigInteger('header_id');
+            $table->foreign('header_id')->references('id')->on('assets')->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropForeign(['header_id']);
+            $table->dropColumn('header_id');
+        });
+    }
+}
