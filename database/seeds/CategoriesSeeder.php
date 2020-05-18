a6efe1a61a1c59b1a1f6f3c71507a4798fd9d2ee
Raul de Heer <raul@supertask.nl>
2020-05-18 13:48:54 +0200

    Add category seeder

---
+++ database/seeds/CategoriesSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Category;
+use Illuminate\Database\Seeder;
+
+class CategoriesSeeder extends Seeder
+{
+    public function run()
+    {
+        $cake = new Category();
+        $cake->category = 'Taart';
+        $cake->type = 'advertisement';
+        $cake->save();
+
+        $subCategory = new Category();
+        $subCategory->category = 'Appeltaart';
+        $subCategory->parent()->associate($cake);
+        $subCategory->save();
+
+        $subCategory = new Category();
+        $subCategory->category = 'Kersentaart';
+        $subCategory->parent()->associate($cake);
+        $subCategory->save();
+
+        $subCategory = new Category();
+        $subCategory->category = 'Zonder suiker';
+        $subCategory->parent()->associate($subCategory);
+        $subCategory->save();
+
+        $labor = new Category();
+        $labor->category = 'Handwerk';
+        $labor->type = 'advertisement';
+        $labor->save();
+
+        $subCategory = new Category();
+        $subCategory->category = 'Tuin';
+        $subCategory->parent()->associate($labor);
+        $subCategory->save();
+
+        $subCategory = new Category();
+        $subCategory->category = 'Grasmaaien';
+        $subCategory->parent()->associate($labor);
+        $subCategory->save();
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
             UsersSeeder::class,
             AssetsSeeder::class,
             BidsSeeder::class,
-            IntakeSeeder::class
+            IntakesSeeder::class,
+            CategoriesSeeder::class
         ]);
     }
 }
