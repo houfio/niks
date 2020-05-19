6163c81470bf494cbeeb51a9dfa9ca24c399569a
houfio <26674115+houfio@users.noreply.github.com>
2020-05-19 15:39:08 +0200

    Add transaction popup

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -9,11 +9,16 @@
     <div class="profile-info">
       <div class="profile-image"></div>
       <span dusk="user_name">{{ $user->getFullName() }}</span>
-      @can('update', $user)
-        <a class="button light small" href="{{ action('UserController@edit', ['user' => $user]) }}">
-          {{ __('views/profile.edit') }}
-        </a>
-      @endcan
+      <div>
+        <button class="button small" data-micromodal-trigger="transaction-modal">
+          Overmaken
+        </button>
+        @can('update', $user)
+          <a class="button light small" href="{{ action('UserController@edit', ['user' => $user]) }}">
+            {{ __('views/profile.edit') }}
+          </a>
+        @endcan
+      </div>
     </div>
   </div>
   @forelse($advertisements as $advertisement)
@@ -24,4 +29,12 @@
     </x-empty>
   @endforelse
   {{ $advertisements->links() }}
+  <x-modal id="transaction" title="Transaction">
+    <form>
+      <x-input name="amount" label="Bedrag"/>
+      <button class="button" type="submit">
+        Overmaken
+      </button>
+    </form>
+  </x-modal>
 @endsection
