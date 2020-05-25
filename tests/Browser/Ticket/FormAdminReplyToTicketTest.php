ed07ad43d6728d2a1a20748ec79f9be42da6807f
Raul de Heer <raul@supertask.nl>
2020-05-25 19:12:54 +0200

    Add test for replying to ticket

---
+++ tests/Browser/Ticket/FormAdminReplyToTicketTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Browser\Ticket;
+
+use App\Ticket;
+use App\TicketType;
+use App\User;
+use Facebook\WebDriver\WebDriverBy;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+use Throwable;
+
+class FormAdminReplyToTicketTest extends DuskTestCase
+{
+    /**
+     * @test
+     * @throws Throwable
+     */
+    public function testAdminReplyToTicket()
+    {
+        $admin = factory(User::class)->create([
+            'is_admin' => true,
+            'is_approved' => true
+        ]);
+
+        $user = factory(User::class)->make();
+
+        /** @var Ticket $ticket */
+        $ticket = factory(Ticket::class)->create([
+            'first_name' => $user->first_name,
+            'last_name' => $user->last_name,
+            'email' => $user->email,
+            'type_id' => 1
+        ]);
+
+        $this->browse(function (Browser $browser) use ($admin, $ticket) {
+            $browser->loginAs($admin)
+                ->visit("/tickets/$ticket->id/edit")
+                ->type('response', 'Test reply for this ticket')
+                ->press('reply')
+                ->assertPathIs("/tickets/$ticket->id/edit");
+
+            $responses = $browser->driver->findElements(WebDriverBy::id('response_item'));
+            $this->assertCount($ticket->responses()->count(), $responses);
+        });
+
+        $this->assertDatabaseHas('ticket_responses', [
+            'ticket_id' => $ticket->id,
+            'response' => 'Test reply for this ticket'
+        ]);
+    }
+}
